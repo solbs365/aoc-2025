539c130da978b83461e7d41324d93815edcf6719
solbs365 <solmybs@hotmail.com>
2026-01-14 16:44:20 +0000

    Add files via upload

---
+++ day09/AoC_9.0/src/main/java/software/aoc2025/day9/app/AreaSolver.java
@@ -0,0 +1,9 @@
+package software.aoc2025.day9.app;
+
+import software.aoc2025.day9.model.Position;
+
+import java.util.List;
+
+public interface AreaSolver {
+    long findMaxArea(List<Position>positions);
+}
